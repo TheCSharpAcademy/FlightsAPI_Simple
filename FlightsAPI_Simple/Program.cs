420e8148b5548a7c2b80534e38b52abbcd759057
Pablo <thecsharpacademy@gmail.com>
2024-05-25 21:19:18 +1000

    Flight Service

--- FlightsAPI_Simple/Program.cs
+++ FlightsAPI_Simple/Program.cs
@@ -1,4 +1,5 @@
 using FlightsAPI_Simple.Data;
+using FlightsAPI_Simple.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +8,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<FlightsDbContext>(opt => opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IFlightService, FlightService>();
 
 var app = builder.Build();
 
---
+++ FlightsAPI_Simple/Services/FlightService.cs
@@ -0,0 +1,68 @@
+using FlightsAPI_Simple.Data;
+using FlightsAPI_Simple.Models;
+
+namespace FlightsAPI_Simple.Services
+{
+    public class FlightService : IFlightService
+    { 
+        private readonly FlightsDbContext _dbContext;
+
+        public FlightService(FlightsDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Flight CreateFlight(Flight flight)
+        {
+            var savedFlight = _dbContext.Flights.Add(flight);
+
+            _dbContext.SaveChanges();
+
+            return savedFlight.Entity;
+        }
+
+        public string? DeleteFlights(int id)
+        {
+            Flight? savedFlight = _dbContext.Flights.Find(id);
+
+            if (savedFlight is null) {
+                return null;
+            }
+
+            _dbContext.Remove(savedFlight);
+
+            return $"Successfully deleted flight with id: {id}";
+        }
+
+        public List<Flight> GetAllFlights()
+        {
+            return _dbContext.Flights.ToList();
+        }
+
+        public Flight? GetFlightById(int id)
+        {
+            return _dbContext.Flights.Find(id);
+        }
+
+        public Flight UpdateFlight(int id, Flight updatedFlight)
+        {
+            Flight savedFlight = _dbContext.Flights.Find(id);
+
+            if (savedFlight is null) {
+                return null;
+            }
+
+            savedFlight.Id = updatedFlight.Id;
+            savedFlight.AirlineName = updatedFlight.AirlineName;
+            savedFlight.DepartureAirportCode = updatedFlight.DepartureAirportCode;
+            savedFlight.ArrivalAirportCode = updatedFlight.ArrivalAirportCode;
+            savedFlight.DepartureDateTime = updatedFlight.DepartureDateTime;
+            savedFlight.ArrivalDateTime = updatedFlight.ArrivalDateTime;    
+            savedFlight.PassengerCapacity = updatedFlight.PassengerCapacity;
+
+            _dbContext.SaveChanges();
+
+            return savedFlight;
+        }
+    }
+}
